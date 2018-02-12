89322de2caa82ebcb67aba88959bd610254dfefd
Maxim Kulkin <maxim.kulkin@gmail.com>
2018-02-12 02:14:01 -0800

    Add support for ESP_IDF SDK

--- component.mk
+++ component.mk
@@ -1,13 +1,22 @@
+wolfssl_VERSION := 3.13.0-stable
 
-wolfssl_VERSION = 3.13.0-stable
-wolfssl_THIRDPARTY_ROOT = $(wolfssl_ROOT)wolfssl-$(wolfssl_VERSION)
+ifdef component_compile_rules
+    # ESP_OPEN_RTOS
+    wolfssl_THIRDPARTY_ROOT := $(wolfssl_ROOT)wolfssl-$(wolfssl_VERSION)
 
-INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
+    INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
 
-wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
-wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
+    wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
+    wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
 
-EXTRA_CFLAGS += -DWOLFSSL_USER_SETTINGS
+    EXTRA_CFLAGS += -DESP_OPEN_RTOS -DWOLFSSL_USER_SETTINGS
 
-$(eval $(call component_compile_rules,wolfssl))
+    $(eval $(call component_compile_rules,wolfssl))
+else
+    # ESP_IDF
+    wolfssl_THIRDPARTY_ROOT := wolfssl-$(wolfssl_VERSION)
+
+    COMPONENT_SRCDIRS = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
+    COMPONENT_ADD_INCLUDEDIRS = . include $(wolfssl_THIRDPARTY_ROOT)
+endif
 
